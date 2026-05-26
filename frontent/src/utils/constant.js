2bedba1b5f4bcff46a66658c4a293b0f07f9e47d
nimasis brine <mauryakaran0542@gmail.com>
2026-05-26 12:22:08 +0530

    Fix: add email-validator, groq, and fix port binding

--- frontent/src/utils/constant.js
+++ frontent/src/utils/constant.js
@@ -1,5 +1,5 @@
 // ✅ Centralized API & Socket URLs
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const USER_API_END_POINT = `${BASE_URL}/user`;
 export const JOB_API_END_POINT = `${BASE_URL}/job`;
