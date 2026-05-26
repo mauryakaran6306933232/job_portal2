3717a0616f769cc0dc8ff482a922abd0d042066b
nimasis brine <mauryakaran0542@gmail.com>
2026-05-26 12:15:17 +0530

    Fix: add email-validator, groq, and fix port binding

--- frontent/src/hooks/useGetAllCompanies.jsx
+++ frontent/src/hooks/useGetAllCompanies.jsx
@@ -1,29 +1,4 @@
-// import axios from 'axios'
-// import { useEffect } from 'react'
-// import { useDispatch } from 'react-redux'
-// import { setAllCompanies } from '../redux/CompanySlice'
 
-// const useGetAllCompanies = () => {
-//   const dispatch = useDispatch();
-
-//   useEffect(() => {
-//     const fetchCompanies = async () => {
-//       try {
-//         const res = await axios.get('http://localhost:8000/company/get', {
-//           withCredentials: true
-//         });
-//         if (res?.data?.success) {
-//           dispatch(setAllCompanies(res?.data?.companies));
-//         }
-//       } catch (error) {
-//         console.log("Error fetching companies:", error);
-//       }
-//     };
-//     fetchCompanies();
-//   }, [dispatch]);
-// };
-
-// export default useGetAllCompanies;
 import axios from 'axios'; import { useEffect } from 'react'; import { useDispatch } from 'react-redux'; import { setAllCompanies } from '../redux/CompanySlice'; import { COMPANY_API_END_POINT } from '../utils/constant'; // ✅ ADD
 const useGetAllCompanies = () => {
   const dispatch = useDispatch();
--- frontent/src/hooks/useGetApplicants.jsx
+++ frontent/src/hooks/useGetApplicants.jsx
@@ -1,36 +1,4 @@
-// import axios from 'axios';
-// import { useEffect } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { setAllApplicants } from '../redux/ApplicationSlice'; // We will create/update this next
 
-// const useGetApplicants = (jobId) => {
-//     const dispatch = useDispatch();
-
-//     useEffect(() => {
-//         const fetchApplicants = async () => {
-//             try {
-//                 const res = await axios.get(
-//                     `http://localhost:8000/api/v1/application/${jobId}/applicants`,
-//                     { withCredentials: true }
-//                 );
-                
-//                 if (res.data.success) {
-//                     // Your backend returns the Job document with the applications populated inside it
-//                     dispatch(setAllApplicants(res.data.job));
-//                 }
-//             } catch (error) {
-//                 console.log("Error fetching applicants:", error);
-//             }
-//         };
-
-//         // Only fetch if jobId is available
-//         if (jobId) {
-//             fetchApplicants();
-//         }
-//     }, [jobId, dispatch]);
-// };
-
-// export default useGetApplicants;
 import axios from 'axios'; import { useEffect } from 'react'; import { useDispatch } from 'react-redux'; import { setAllApplicants } from '../redux/ApplicationSlice'; import { API_V1 } from '../utils/constant'; // ✅ ADD
 const useGetApplicants = (jobId) => {
   const dispatch = useDispatch();
